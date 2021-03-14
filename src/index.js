ce3784cb5bfe322a642b5932fc167c5780b40361
オノヤスヒロ <47861810+cant-dev@users.noreply.github.com>
2021-03-14 15:15:23 +0900

    DOM・リストの生成

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 import "./styles.css";
+
+const onclickAdd = () => {
+  // Textboxの値を初期化し、値を初期化する
+  const inputText = document.getElementById("add-text").value;
+  // alert(inputText);
+  document.getElementById("add-text").value = "";
+
+  // JS上からHTMLのDOMを生成して差し込む;
+
+  // divの生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  console.log(div);
+
+  // listの生成
+  // createElement関数はDOMの生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+  console.log(li);
+
+  div.appendChild(li);
+  console.log(div);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+// divの子要素にliを入れる
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onclickAdd());
